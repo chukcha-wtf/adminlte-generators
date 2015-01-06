f62b2b210147c6c6c933f781e789adaf1c4c0e1d
Pavlo Babenko <pavlo.babenko@gmail.com>
2015-01-06 16:36:43 +0200

    Adding breadcrumbs

--- Gemfile
+++ Gemfile
@@ -5,3 +5,4 @@ gemspec
 
 gem 'rails', ">= 4.0.0"
 gem 'adminlte', ">= 1.3.0.1"
+gem 'sass-rails'
--- lib/adminlte-generators.rb
+++ lib/adminlte-generators.rb
@@ -3,6 +3,11 @@ require "rails"
 module Adminlte
   module Generators
     class Engine < ::Rails::Engine
+      config.app_generators.stylesheets false
+
+      # config.app_generators do |g|
+      #   g.templates.unshift File::expand_path('../templates', __FILE__)
+      # end
     end
   end
 end
