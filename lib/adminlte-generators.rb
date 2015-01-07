685f2998717cfc8ebd1549567ce779eada8b0c13
Pavlo Babenko <pavlo.babenko@gmail.com>
2015-01-07 22:19:46 +0200

    Adding templates unshift path

--- lib/adminlte-generators.rb
+++ lib/adminlte-generators.rb
@@ -2,13 +2,17 @@ require "rails"
 
 module Adminlte
   module Generators
-    class Engine < ::Rails::Engine
+    class Engine < Rails::Engine
       config.app_generators.stylesheets false
 
       initializer 'adminlte-generators.setup', group: :all do |app|
         app.config.assets.paths << ::Rails.root.join('app', 'assets', 'fonts')
 
         app.config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+      end
+
+      config.app_generators do |g|
+        g.templates.unshift File::expand_path('../templates', __FILE__)
       end
     end
   end
